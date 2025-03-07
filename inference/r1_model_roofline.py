04cdcad4ae8be852609adeb04c0c01681c21870e
wangshaojie05 <wangshaojie05@kuaishou.com>
2025-03-08 00:03:46 +0800

    add e2e

---
+++ inference/r1_model_roofline.py
@@ -0,0 +1,56 @@
+import numpy as np
+from prettytable import PrettyTable
+from utils import *
+
+# 设置模型参数
+hidden_dim = 7168
+h_q = 1536
+h_c = 512
+n_h = 128
+h_d = 128
+h_d_r = 64
+tp = 1
+expert_hidden_dim = 2048
+num_experts = 256
+topk = 8
+num_shared_experts = 1
+num_layers = 61
+
+context_lens = [4096] #, 5120, 6144, 8192, 16384, 32768, 65536, 102400]
+
+eps = [8] # [8, 16, 32, 64, 128]
+
+table = PrettyTable()
+
+model_stable_size = 14.11 * 1024 ** 3
+model_dist_size = 609 * 1024 ** 3
+kvcache_size_per_token = 70272
+
+table.field_names = ["ai chip name", "context length", "ep", "max_batch_size", "min_time_s_per_batch", "max_decode_tps"]
+
+
+for ep in eps:
+    for context_len in context_lens:
+        for hw in hws:
+            comm_bw = hw.nvl_bandwidth if ep <= hw.nvl_num else hw.ib_bandwidth
+            max_bsz = int((hw.hbm_size - model_stable_size - model_dist_size / ep) / (kvcache_size_per_token * context_len)) * ep
+            time_ms_mla0_s_dispatch = max(
+                comm_time_ms(max_bsz, hidden_dim, ep, topk, comm_bw, 1),
+                shared_expert_time_ms(max_bsz, hidden_dim, ep, expert_hidden_dim, num_shared_experts, hw.peak_flops, hw.memory_bandwidth) + 
+                mla0_time_ms(max_bsz / ep, hidden_dim, h_q, h_c, h_d, h_d_r, n_h, hw.peak_flops, hw.memory_bandwidth)
+            )
+            print(time_ms_mla0_s_dispatch)
+            time_ms_moe = moe_time_ms(max_bsz, hidden_dim, expert_hidden_dim, topk, ep, num_experts, hw.peak_flops, hw.memory_bandwidth)
+            print(time_ms_moe)
+            time_ms_mla1_combine = max(
+                comm_time_ms(max_bsz, hidden_dim, ep, topk, comm_bw, 2),
+                mla1_time_ms(max_bsz / ep, context_len, n_h, h_d, h_d_r, h_c, hidden_dim, hw.peak_flops / 2, hw.memory_bandwidth)
+            )
+            print(time_ms_mla1_combine)
+            time_ms = (time_ms_mla0_s_dispatch + time_ms_moe + time_ms_mla1_combine) * num_layers
+            tokens_total = 1 / time_ms * max_bsz
+            table.add_row([hw.hw_name, context_len, ep, max_bsz, time_ms, tokens_total])
+
+table.title = "performance comparison"
+print(table)
+
--- inference/utils.py
+++ inference/utils.py
@@ -13,17 +13,53 @@ def ops_per_gemm(b, m, n, k):
     return b * m * n * k * 2
 
 def mem_acc_size_per_attn(b, nh_q, nh_kv, s_q, s_kv, hd_qk, hd_v, ele_size):
-    return b * (nh_q * s_q * hd_qk * 2 + nh_kv * s_kv * hd_qk + nh_kv * s_kv * hd_v) * ele_size
+    return b * (nh_q * s_q * hd_qk * 2 + nh_kv * s_kv * hd_qk + 0 * nh_kv * s_kv * hd_v) * ele_size
 
 def ops_per_attn(b, nh_q, s_q, s_kv, hd_qk, hd_v):
-    return b * nh_q * (s_q * s_kv * hd_qk + s_kv * s_kv * hd_v) * 2
+    return b * nh_q * (s_q * s_kv * hd_qk + s_q * s_kv * hd_v) * 2
+
+def comm_time_ms(bs, h, ep, topk, ib_bw, ele_size):
+    return ele_size * topk * bs * h / ep / ib_bw
+
+def shared_expert_time_ms(bs, h, dp, e, num_shared, tflops, hbm_bw):
+    comp_ms = (ops_per_gemm(1, bs / dp, e * 2 * num_shared, h) + ops_per_gemm(num_shared, bs / dp, h * num_shared, e)) / tflops
+    mem_ms = (mem_acc_size_per_gemm(1, bs / dp, e * 2 * num_shared, h, 1) + mem_acc_size_per_gemm(num_shared, bs / dp, h * num_shared, e, 1)) / hbm_bw
+    return max(comp_ms, mem_ms)
+
+def mla0_time_ms(bs, h, h_q, h_c, h_d, h_dr, nh, tflops, hbm_bw):
+    comp_ms = (ops_per_gemm(1, bs, h_q + h_c + h_dr, h) + 
+               ops_per_gemm(1, bs, nh * (h_d + h_dr), h_q) + 
+               ops_per_gemm(nh, bs, h_c, h_d)) / tflops
+    mem_ms = (mem_acc_size_per_gemm(1, bs, h_q + h_c + h_dr, h, 1) + 
+              mem_acc_size_per_gemm(1, bs, nh * (h_d + h_dr), h_q, 1) + 
+              mem_acc_size_per_gemm(nh, bs, h_c, h_d, 1)) / hbm_bw
+    return max(comp_ms, mem_ms)
+
+def mla1_time_ms(bs, context_len, nh, h_d, h_dr, h_c, h, tflops, hbm_bw):
+    attn_comp_ms = ops_per_attn(bs, nh, 1, context_len, h_c + h_dr, h_c) / tflops
+    print(f"attn_comp_ms={attn_comp_ms}")
+    attn_mem_ms = mem_acc_size_per_attn(bs, nh, 1, 1, context_len, h_c + h_dr, h_c, 2) / hbm_bw
+    print(f"attn_mem_ms={attn_mem_ms}")
+    absorption_gemm_comp_ms = ops_per_gemm(nh, bs, h_c, h_d) / tflops
+    absorption_gemm_mem_ms = mem_acc_size_per_gemm(nh, bs, h_c, h_d, 1) / hbm_bw
+    o_proj_gemm_comp_ms = ops_per_gemm(1, bs, nh * h_d, h) / tflops
+    o_proj_gemm_mem_ms = mem_acc_size_per_gemm(1, bs, nh * h_d, h, 1) / hbm_bw
+    return max(attn_comp_ms, attn_mem_ms) + max(absorption_gemm_comp_ms, absorption_gemm_mem_ms) + max(o_proj_gemm_comp_ms, o_proj_gemm_mem_ms)
+
+def moe_time_ms(bs, h, e, topk, ep, num_experts, tflops, hbm_bw):
+    up_gemm_comp_ms = ops_per_gemm(1, topk * bs // ep, e * 2, h) / tflops
+    up_gemm_mem_ms = mem_acc_size_per_grouped_gemm(topk, num_experts // ep, bs // ep, e * 2, h, 1) / hbm_bw
+    print(f"up_gemm_comp_ms={up_gemm_comp_ms}, up_gemm_mem_ms={up_gemm_mem_ms}")
+    down_gemm_comp_ms = ops_per_gemm(1, topk * bs // ep, h, e) / tflops
+    down_gemm_mem_ms = mem_acc_size_per_grouped_gemm(topk, num_experts // ep, bs // ep, h, e, 1) / hbm_bw
+    return max(up_gemm_comp_ms, up_gemm_mem_ms) + max(down_gemm_comp_ms, down_gemm_mem_ms)
 
 def plot_roofline(bsz, perfs, passing_points, hws):
     # 创建图形
     plt.figure(figsize=(10, 6))
     # plt.loglog(OI, performance, 'b-', linewidth=2, label='Roofline')+
 
-    colors = ['green', 'red', 'blue', 'black']
+    colors = ['green', 'red', 'blue', 'black', 'gray']
 
     for idx, perf in enumerate(perfs):
         plt.plot(bsz, perf, '-', color=colors[idx], linewidth=2, label=f'Roofline of {hws[idx].hw_name}')
@@ -87,17 +123,21 @@ from dataclasses import dataclass
 @dataclass
 class HW_DATA(object):
     hw_name: str
+    hbm_size: float
     peak_flops: float
     memory_bandwidth: float
     nvl_bandwidth: float
+    nvl_num: int
     ib_bandwidth: float
 
     def __post_init__(self):
         self.critical_OI = self.peak_flops / self.memory_bandwidth
 
 hws = [
-    HW_DATA("MI308x", 464e12, 5300e9, 896e9, 50e9), 
-    HW_DATA("H100-SGM", 1900e12, 3300e9, 900e9, 50e9),
-    HW_DATA("H200", 1900e12, 4800e9, 900e9, 50e9),
+    #HW_DATA("MI308x", 192 * 1024 ** 3, 464e12, 5300e9, 448e9, 8, 50e9), 
+    #HW_DATA("H800-SGM", 80 * 1024 ** 3, 1900e12, 3300e9, 200e9, 8, 50e9),
+    HW_DATA("H200", 141 * 1024 ** 3, 1900e12, 4800e9, 450e9, 8, 50e9),
+    #HW_DATA("910B", 64 * 1024 ** 3, 750e12, 1600e9, 196e9, 8, 50e9),
+    #HW_DATA("910C", 128 * 1024 ** 3, 1500e12, 3200e9, 392e9, 384, 50e9),
     ]
 
